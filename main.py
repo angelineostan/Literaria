6fc0bcf1b15cf02467cbd91da219ac5b08cad2b2
Angeline Ostan <111232307+angelineostan@users.noreply.github.com>
2025-08-24 01:35:45 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask,  render_template
 
 app = Flask(__name__, 
-            template_folder='../template',
-            static_folder='../static',
+            template_folder='template',
+            static_folder='static',
             static_url_path='/static')
 app.secret_key = 'supersecretkey'
 
@@ -12,15 +12,23 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('homepage/about.html')
+    return render_template('homepage/aboutus.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('homepage/contact.html')
+    return render_template('homepage/contactus.html')
 
 @app.route('/login')
-def login():
+def role_login():
     return render_template('auth/login.html')
+
+@app.route('/signup')
+def role_signup():
+    return render_template('role/signup_role_select.html')
+
+@app.route('/signup_role_select')
+def student_selected():
+    return render_template('auth/student_signup.html')
 
 @app.route('/role_select')
 def role_select():
